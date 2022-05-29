c48a4b02b849868f87e25103bf50afd503f0382f
a00700c <a00700c@gmail.com>
2022-05-30 03:04:55 +0900

    feat: main.cpp, shopping.h 작성

--- Hw3/Implementation/김준모/main.cpp
+++ Hw3/Implementation/김준모/main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <string.h>
 #include "shopping.h"
@@ -8,6 +10,14 @@
 
 void doTask();
 void join();
+void withdraw();
+void login();
+void logout();
+void registerItem();
+void search();
+void buy();
+void checkBuyList();
+void rate();
 void program_exit();
 
 FILE* in_fp, * out_fp;
@@ -15,8 +25,8 @@ FILE* in_fp, * out_fp;
 int main()
 {
 	// 파일 입출력을 위한 초기화
-	FILE* in_fp = fopen(INPUT_FILE_NAME, "r+");
-	FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+	in_fp = fopen(INPUT_FILE_NAME, "r+");
+	out_fp = fopen(OUTPUT_FILE_NAME, "w+");
 
 	doTask();
 
@@ -37,23 +47,143 @@ void doTask()
 		// 메뉴 구분 및 해당 연산 수행
 		switch (menu_level_1)
 		{
-			case 1:
+		case 1:
+		{
+			switch (menu_level_2)
 			{
-				switch (menu_level_2)
-				{
-					case 1: // "1.1. 회원가입" 메뉴 부분
-					{
-						// join() 함수에서 해당 기능 수행
-						join();
+			case 1: // "1.1. 회원가입" 메뉴 부분
+			{
+				// join() 함수에서 해당 기능 수행
+				join();
 
-						break;
-					}
-					case 2:
-					{
-						break;
-					}
-				}
+				break;
+			}
+			case 2:
+			{
+				withdraw();
+
+				break;
+			}
 			}
 		}
+		case 2:
+		{
+			switch (menu_level_2)
+			{
+			case 1:
+			{
+				login();
+
+				break;
+			}
+			case 2:
+			{
+				logout();
+
+				break;
+			}
+			}
+		}
+		case 3:
+		{
+			switch (menu_level_2)
+			{
+			case 1:
+			{
+				registerItem();
+
+				break;
+			}
+			}
+		}
+
+		case 4:
+		{
+			switch (menu_level_2)
+			{
+			case 1:
+			{
+				search();
+
+				break;
+			}
+			case 2:
+			{
+				buy();
+
+				break;
+			}
+			case 3:
+			{
+				checkBuyList();
+
+				break;
+			}
+			case 4:
+			{
+				rate();
+
+				break;
+			}
+			}
+		}
+		case 6:
+		{
+			switch (menu_level_2)
+			{
+			case 1:
+			{
+				program_exit();
+				is_program_exit = 1;
+				break;
+			}
+			}
+		}
+		}
 	}
+}
+
+void join()
+{}
+void withdraw()
+{}
+void login()
+{}
+void logout()
+{}
+void registerItem()
+{}
+
+
+
+void search()
+{
+
+	char itemName[MAX_STRING];
+
+	fscanf(in_fp, "%s", itemName);
+
+
+
+	fprintf(out_fp, "4.1. 상품 정보 검색\n");
+	fprintf(out_fp, "%s\n", itemName);
+}
+
+void buy()
+{
+
+}
+void checkBuyList()
+{
+
+}
+void rate()
+{
+
+}
+
+
+void program_exit()
+{
+	fprintf(out_fp, "6.1. 종료\n");
 }
--- Hw3/Implementation/김준모/shopping.h
+++ Hw3/Implementation/김준모/shopping.h
@@ -58,3 +58,51 @@ public:
 	int* getBuyItemList();
 	~BuyItemList();
 }; 
+
+
+// Class : SearchItemUI
+// Description : boundary class when user searches item
+// Created : 2022/5/30 00:00 am
+// Author : ±èÁØ¸ð
+// mail : a00700c@g.hongik.ac.kr
+class SearchItemUI
+{
+public:
+	void searchItemByName(string itemName);
+};
+
+
+// Class : SearchItem
+// Description : control class that searches item
+// Created : 2022/5/30 00:01 am
+// Author : ±èÁØ¸ð
+// mail : a00700c@g.hongik.ac.kr
+class SearchItem
+{
+public:
+	ItemInfo showItemInfo(string itemName);
+};
+
+
+// Class : BuyItemUI
+// Description : boundary class when user buys item
+// Created : 2022/5/30 00:03 am
+// Author : ±èÁØ¸ð
+// mail : a00700c@g.hongik.ac.kr
+class BuyItemUI
+{
+public:
+	void buyItem(string itemName);
+};
+
+
+// Class : BuyItem
+// Description : control class that buys item
+// Created : 2022/5/30 00:07 am
+// Author : ±èÁØ¸ð
+// mail : a00700c@g.hongik.ac.kr
+class BuyItem
+{
+public:
+	void buyItem(string itemName);
+};
